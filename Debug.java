ba2f9207251517e2610f81b7045076147ce6067a
contacto@fernandodpr.es <contacto@fernandodpr.es>
2022-11-29 13:41:57 +0100

    Clase debug

---
+++ Debug.java
@@ -0,0 +1,10 @@
+public class Debug{
+    public static void info(Object parametro){
+        System.out.println("INFO:  " + parametro);
+        return;
+    }
+    public static void taco(Object parametro){
+        System.out.println("BORRA ESTO ANTES DE ENTREGAR:  " + parametro);
+        return;
+    }
+}
