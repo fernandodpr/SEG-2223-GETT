c07eb929647de199cb7a338ca43b07097e9317d6
contacto@fernandodpr.es <contacto@fernandodpr.es>
2022-11-29 11:32:30 +0100

    Creación de la clase archivo.

--- Archivo.java
+++ Archivo.java
@@ -1,53 +1,71 @@
-public class  Archivo{
+import java.io.*;
+import java.util.Date;
+
+public class  Archivo implements Serializable {
     private String numeroRegistro;
     private String timestamp;
-    private byte[] documento;
+    private byte[] documento;  //esto se guarda cifardo
+    private boolean cifrado;
+    private String nombreDocumento;
     private byte[] firma;
-    private byte[] firma_rgistrador;
+    private byte[] firma_registrador;
 
+	//GETTERS Y SETTERS
+
+	public boolean isCifrado() {
+		return this.cifrado;
+	}
+	public void setCifrado(boolean cifrado) {
+		this.cifrado = cifrado;
+    }
 	public String getNumeroRegistro() {
 		return this.numeroRegistro;
 	}
-
 	public void setNumeroRegistro(String numeroRegistro) {
 		this.numeroRegistro = numeroRegistro;
 	}
-
 	public String getTimestamp() {
 		return this.timestamp;
 	}
-
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
-
 	public byte[] getDocumento() {
 		return this.documento;
 	}
-
 	public void setDocumento(byte[] documento) {
 		this.documento = documento;
 	}
-
+	public String getNombreDocumento() {
+		return this.nombreDocumento;
+	}
+	public void setNombreDocumento(String nombreDocumento) {
+		this.nombreDocumento = nombreDocumento;
+	}
 	public byte[] getFirma() {
 		return this.firma;
 	}
-
 	public void setFirma(byte[] firma) {
 		this.firma = firma;
 	}
-
-	public byte[] getFirma_rgistrador() {
-		return this.firma_rgistrador;
+	public byte[] getFirma_registrador() {
+		return this.firma_registrador;
+	}
+	public void setFirma_registrador(byte[] firma_registrador) {
+		this.firma_registrador = firma_registrador;
 	}
 
-	public void setFirma_rgistrador(byte[] firma_rgistrador) {
-		this.firma_rgistrador = firma_rgistrador;
-	}
+	//Métodos para cifrar
 
-
-    public Archivo() {
-        
+	//Métodos para comprobar firma
+    public Archivo(byte[] documento,String nombreDocumento) {
+		this.numeroRegistro= null;
+		this.timestamp=String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
+		this.documento=documento;
+		this.cifrado=false;
+		this.nombreDocumento=nombreDocumento;
+		this.firma=null;
+		this.firma_registrador=null;
     }
     
 }
